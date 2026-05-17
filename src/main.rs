8666dd99a80d4ac01ac3720566ff1799372c3184
Daniel Feather <12731372+danielfeather@users.noreply.github.com>
2026-05-17 21:48:42 +0100

    Added uploading and ability to list bucket contents

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #[cfg(not(feature = "debug"))]
 use assets::Manifest;
+use aws_config::{
+    BehaviorVersion, Region, meta::region::RegionProviderChain, sts::AssumeRoleProvider,
+};
+use aws_sdk_s3::Client;
 use axum::{
-    Router, debug_handler,
+    Router,
     extract::State,
     http::StatusCode,
     response::Html,
@@ -16,7 +20,7 @@ use tower_sessions::{
     cookie::{Key, time::Duration},
 };
 
-use crate::{flash::Flash, session::FileSessionStorage};
+use crate::session::FileSessionStorage;
 
 mod assets;
 mod flash;
@@ -71,10 +75,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn home(
-    State(state): State<Arc<AppState>>,
-    session: Session,
-) -> axum::response::Result<Html<String>> {
+async fn home(State(state): State<Arc<AppState>>) -> axum::response::Result<Html<String>> {
     let scripts = assets::resolve_scripts(
         Path::new("client/main.ts"),
         #[cfg(not(feature = "debug"))]
@@ -91,12 +92,50 @@ async fn home(
         None,
     );
 
+    let role_arn = std::env::var("IAM_ROLE")
+        .map_err(|_| (StatusCode::SERVICE_UNAVAILABLE, "Service is misconfigured"))?;
+
+    let region_provider = RegionProviderChain::first_try(Region::new("us-west-2"));
+
+    let shared_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+
+    let provider = AssumeRoleProvider::builder(role_arn)
+        .configure(&shared_config)
+        .session_name("testAR")
+        .build()
+        .await;
+
+    let local_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(Region::new("eu-west-2"))
+        .credentials_provider(provider)
+        .load()
+        .await;
+
+    let client = Client::new(&local_config);
+
+    let bucket_name = std::env::var("S3_BUCKET")
+        .map_err(|_| (StatusCode::SERVICE_UNAVAILABLE, "Service is misconfigured"))?;
+
+    let response = client
+        .list_objects_v2()
+        .bucket(bucket_name)
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let objects = response.contents();
+
+    let receipts: Vec<&str> = objects.iter().filter_map(|ob| ob.key()).collect();
+
     let env = state.loader.acquire_env().unwrap();
 
     let templ = env.get_template("home.html").unwrap();
 
     let res = templ
-        .render(context! { css => css, scripts => scripts })
+        .render(context! { css => css, scripts => scripts, receipts => receipts })
         .unwrap();
 
     Ok(Html(res))
--- src/upload.rs
+++ src/upload.rs
@@ -1,28 +1,107 @@
-use std::{path::Path, sync::Arc};
+use std::{fmt::Write as _, path::Path, sync::Arc};
 
+use aws_config::{
+    BehaviorVersion, environment::EnvironmentVariableCredentialsProvider,
+    meta::region::RegionProviderChain, sts::AssumeRoleProvider,
+};
+use aws_sdk_s3::{
+    Client,
+    config::{ProvideCredentials, Region},
+    primitives::{ByteStream, SdkBody},
+};
 use axum::{
+    body::Bytes,
     extract::{Multipart, State},
     http::StatusCode,
-    response::{Html, IntoResponse, Redirect, Response},
+    response::{ErrorResponse, Html, IntoResponse, Redirect, Response},
 };
 use minijinja::context;
 use tower_sessions::Session;
+use uuid::Uuid;
 
-use crate::{AppState, assets, flash::Flash};
+use crate::{AppState, assets};
 
 pub async fn upload(
     State(state): State<Arc<AppState>>,
     session: Session,
     mut multipart: Multipart,
 ) -> axum::response::Result<Redirect> {
-    while let Some(mut field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
+    let mut name: Option<String> = None;
+    let mut data: Option<Bytes> = None;
 
-        println!("Length of `{}` is {} bytes", name, data.len());
+    while let Some(mut field) = multipart.next_field().await? {
+        let field_name = field.name().unwrap().to_string();
+
+        if field_name == "name" {
+            name = Some(field.text().await?);
+            continue;
+        }
+
+        if field_name == "file" {
+            data = Some(field.bytes().await?);
+        }
     }
 
-    let _ = session.insert("filename", uuid::Uuid::now_v7()).await;
+    let role_arn = std::env::var("IAM_ROLE")
+        .map_err(|_| (StatusCode::SERVICE_UNAVAILABLE, "Service is misconfigured"))?;
+
+    let region_provider = RegionProviderChain::first_try(Region::new("us-west-2"));
+
+    let shared_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+
+    let provider = AssumeRoleProvider::builder(role_arn)
+        .configure(&shared_config)
+        .session_name("testAR")
+        .build()
+        .await;
+
+    let local_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(Region::new("eu-west-2"))
+        .credentials_provider(provider)
+        .load()
+        .await;
+
+    let client = Client::new(&local_config);
+
+    let bucket_name = std::env::var("S3_BUCKET")
+        .map_err(|_| (StatusCode::SERVICE_UNAVAILABLE, "Service is misconfigured"))?;
+
+    let mut filename = Uuid::now_v7().to_string();
+
+    if let Some(name) = name {
+        if name.len() > 255 {
+            return Err((StatusCode::BAD_REQUEST, "Bad request").into());
+        }
+
+        let _ = write!(&mut filename, "-{name}");
+    }
+
+    let Some(body) = data else {
+        return Err((StatusCode::BAD_REQUEST, "No receipt provided").into());
+    };
+
+    client
+        .put_object()
+        .bucket(bucket_name)
+        .key(&filename)
+        .body(body.into())
+        .send()
+        .await
+        .map_err(|e| {
+            println!("{e:?}");
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to upload receipt",
+            )
+        })?;
+
+    match session.insert("filename", filename).await {
+        Err(e) => println!("{e:?}"),
+        _ => {}
+    }
 
     Ok(Redirect::to("/success"))
 }
@@ -48,7 +127,7 @@ pub async fn success(
     );
 
     let filename = session
-        .remove::<uuid::Uuid>("filename")
+        .remove::<String>("filename")
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
